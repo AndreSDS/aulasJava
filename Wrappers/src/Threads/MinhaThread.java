99d69eeab3190cd8dd9b08cabea2279f92090a83
AndréSDS <andre_smiths@outlook.com>
2018-04-16 21:11:26 -0300

    ExercicioThreads

--- Wrappers/src/Threads/MinhaThread.java
+++ Wrappers/src/Threads/MinhaThread.java
@@ -4,12 +4,14 @@ public class MinhaThread implements Runnable{
 
     private String nome;
     private int tempo;
+    private boolean foiTerminada;
+    private boolean estaSuspensa;
 
     public MinhaThread(String nome, int tempo){
         this.nome = nome;
+        this.estaSuspensa = false;
         this.tempo = tempo;
-        //Thread t = new Thread(this);
-        //t.start();
+        new Thread(this,nome).start();
     }
 
     @Override
@@ -18,10 +20,31 @@ public class MinhaThread implements Runnable{
             for (int i = 0; i < 6; i++){
                 System.out.println(nome + " contador "+ i);
                 Thread.sleep(tempo);
+                synchronized (this){
+                    while (estaSuspensa){
+                     wait();
+                    }
+                    if (this.foiTerminada){
+                        break;
+                    }
+                }
             }
         }catch (InterruptedException e){
             e.printStackTrace();
         }
         System.out.println(nome+" Terminou execução");
+    }
+    void suspend(){
+        this.estaSuspensa = true;
+    }
+
+    synchronized void resumed(){
+        this.estaSuspensa = false;
+        notify();
+    }
+
+    synchronized void stop(){
+        this.foiTerminada = true;
+        notify();
     }
 }
--- Wrappers/src/Threads/Teste.java
+++ Wrappers/src/Threads/Teste.java
@@ -4,27 +4,30 @@ public class Teste {
     public static void main(String[] args) {
 
         MinhaThread thread = new MinhaThread("Thread 1", 600);
-        Thread t1 = new Thread(thread);
 
-        MinhaThread thread2 = new MinhaThread("Thread 2", 900);
-        Thread t2 = new Thread(thread2);
+        MinhaThread thread1 = new MinhaThread("Thread 2", 900);
 
-        MinhaThread thread3 = new MinhaThread("thread 3", 300);
-        Thread t3 = new Thread(thread3);
-
-        t1.start();
-        t1.setPriority(Thread.MAX_PRIORITY);
-        t2.start();
-        t3.start();
-
+        thread.suspend();
 
         try {
-            t1.join();
-            t2.join();
-            t3.join();
-        }catch (InterruptedException e){
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        thread1.suspend();
+
+        thread.resumed();
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        thread1.resumed();
+
+        thread1.stop();
 
         System.out.println("Programa finalizado");
     }
