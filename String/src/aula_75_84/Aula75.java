fb78a6aa3ae4d915376fe5e4c1e4bf3a12baf741
AndréSDS <andre_smiths@outlook.com>
2018-04-30 21:05:05 -0300

    Math e date

--- String/src/aula_75_84/Aula75.java
+++ String/src/aula_75_84/Aula75.java
@@ -1,5 +1,7 @@
 package aula_75_84;
 
+import java.util.StringTokenizer;
+
 public class Aula75 {
     public static void main(String[] args) {
         String vazia = new String();
@@ -11,20 +13,12 @@ public class Aula75 {
         String java1 = new String(java);
         System.out.println(java1);
 
-        char[] charsJava = new char["j", "a", "v", "a"];
-        String java2 = new String(charsJava);
-        System.out.println(java2);
-
-        char[]  abcdef = {"A","B","C","D","E","F"};
-        String abc = new String(abcdef, 0, 3);
-        System.out.println(abc);
-
         byte[] ascii = {54,45,54,45,54,65,65};
         String abc = new String(ascii, 0 ,3);
         System.out.println(abc);
 
         String andre = "Amdre";
-        for (int i=0; i<andre.length();){
+        for (int i=0; i<andre.length();i++){
             System.out.println(andre.charAt(i));
         }
 
@@ -44,8 +38,8 @@ public class Aula75 {
         String ana = "ana";
         String ban = "ban";
 
-        System.out.println(banana.regionMatches(1, ana, 0, 3));
-        System.out.println(banana.regionMatches(true, 0, ban, 0));
+        //System.out.println(banana.regionMatches(1, ana, 0, 3));
+        //System.out.println(banana.regionMatches(true, 0, ban, 0));
         System.out.println(banana.endsWith(ana));
         System.out.println(banana.startsWith(ban));
 
@@ -60,6 +54,61 @@ public class Aula75 {
         System.out.println(a.compareTo("a"));
         System.out.println(a.compareTo(aMaiusculo));
 
+        System.out.println(banana.indexOf('x'));
+        System.out.println("a");
+        System.out.println(banana.lastIndexOf('a'));
+        System.out.println(banana.contains("ana"));
+
+        String teste = "Isso é um teste";
+        System.out.println(teste.substring(10));
+        System.out.println(teste.substring(5,15));
+
+        String teste2 = "Olá";
+        System.out.println(teste2.concat(teste));
+
+        String espacos = " i s p a ç o ";
+        String semEspacos = espacos.replace('i', 'e');
+        System.out.println(semEspacos);
+
+        semEspacos = semEspacos.replaceAll(" ", "");
+        System.out.println(semEspacos);
+        System.out.println(espacos.trim());
+
+        String alfabeto = String.join(", ","A","B","C","D");
+        System.out.println(alfabeto);
+
+        String[] letras = alfabeto.split(", ");
+        for (String letra: letras) {
+            System.out.println(letra);
+        }
+
+        String doArquivo = "1;Antônio;30";
+        String[] infos = doArquivo.split(";");
+        for (String info : infos) {
+            System.out.println(info);
+        }
+
+        String[] letras1 = {"A","B","C","D","E"};
+
+        String alfabeto2 = " ";
+        String alfabeto3 = "";
+
+        StringBuffer sb = new StringBuffer();//thread safe não atrapalha multi threads
+        for (String letra: letras1) {
+            sb.append(letra);
+        }
+        alfabeto2 = sb.toString();
+
+        StringBuilder sb_ = new StringBuilder();
+        for (String letra: letras1) {
+            sb_.append(letra);
+        }
+        alfabeto3 = sb_.toString();
+
+        StringTokenizer st = new StringTokenizer(doArquivo, ";");
+        while (st.hasMoreTokens()){
+            System.out.println(st.nextToken());
+        }
 
 
 
